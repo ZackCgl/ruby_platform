9ed10c5127eb37a515ae2bc6d96e1b3814ae0d61
ZackCgl <100427794+ZackCgl@users.noreply.github.com>
2022-09-12 15:30:42 +0200

    firebase secret

--- Components/StateGlobal/db/firebase.js
+++ Components/StateGlobal/db/firebase.js
@@ -5,7 +5,7 @@ import { getStorage, ref } from "firebase/storage";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBF4Cm3fwt3ELCgZmr_nvONDyhM-FCBaoY",
+  apiKey: process.env.FIREBASE_SECRET,
   authDomain: "ruby-8742f.firebaseapp.com",
   projectId: "ruby-8742f",
   storageBucket: "ruby-8742f.appspot.com",
