39b26d3ffb73de71730e48e4b37d719901a82d01
Mark Teekens <mark@rubyfinance.nl>
2022-09-14 17:28:23 +0200

    fs

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google";
-
+import { FirestoreAdapter } from "@next-auth/firebase-adapter"
 export default NextAuth({
    
   // Configure one or more authentication providers
@@ -15,9 +15,11 @@ export default NextAuth({
               response_type: "code"
             }
           }
-      })
+      }),
+
     // ...add more providers here
   ],
+  
   callbacks: {
     session({ session, token, user }) {
       return session // The return type will match the one returned in `useSession()`
